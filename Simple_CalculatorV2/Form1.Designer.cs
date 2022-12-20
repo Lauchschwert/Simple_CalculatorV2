1f0459290d0a62897736907f41dc3220a8c9d707
Niclas <niclas.arnoldner@gmail.com>
2022-12-20 22:58:03 +0100

    Added Textbox
    Not fully functional yet

--- Simple_CalculatorV2/Form1.Designer.cs
+++ Simple_CalculatorV2/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.myButton1 = new Simple_CalculatorV2.MyButton();
+            this.myButton2 = new Simple_CalculatorV2.MyButton();
+            this.myButton3 = new Simple_CalculatorV2.MyButton();
+            this.myButton4 = new Simple_CalculatorV2.MyButton();
+            this.round1 = new Simple_CalculatorV2.round();
             this.SuspendLayout();
             // 
             // myButton1
@@ -44,11 +48,81 @@
             this.myButton1.Location = new System.Drawing.Point(13, 13);
             this.myButton1.Margin = new System.Windows.Forms.Padding(4);
             this.myButton1.Name = "myButton1";
-            this.myButton1.Size = new System.Drawing.Size(193, 57);
+            this.myButton1.Size = new System.Drawing.Size(215, 69);
             this.myButton1.TabIndex = 0;
-            this.myButton1.Text = "myButton1";
+            this.myButton1.Text = "Add";
             this.myButton1.TextColor = System.Drawing.Color.White;
             this.myButton1.UseVisualStyleBackColor = false;
+            this.myButton1.Click += new System.EventHandler(this.myButton1_Click_1);
+            // 
+            // myButton2
+            // 
+            this.myButton2.BackColor = System.Drawing.Color.MediumPurple;
+            this.myButton2.BackgroundColor = System.Drawing.Color.MediumPurple;
+            this.myButton2.BorderColor = System.Drawing.Color.Black;
+            this.myButton2.BorderRadius = 40;
+            this.myButton2.BorderSize = 1;
+            this.myButton2.FlatAppearance.BorderSize = 0;
+            this.myButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.myButton2.ForeColor = System.Drawing.Color.White;
+            this.myButton2.Location = new System.Drawing.Point(13, 90);
+            this.myButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.myButton2.Name = "myButton2";
+            this.myButton2.Size = new System.Drawing.Size(215, 69);
+            this.myButton2.TabIndex = 1;
+            this.myButton2.Text = "Substract";
+            this.myButton2.TextColor = System.Drawing.Color.White;
+            this.myButton2.UseVisualStyleBackColor = false;
+            this.myButton2.Click += new System.EventHandler(this.myButton2_Click);
+            // 
+            // myButton3
+            // 
+            this.myButton3.BackColor = System.Drawing.Color.MediumPurple;
+            this.myButton3.BackgroundColor = System.Drawing.Color.MediumPurple;
+            this.myButton3.BorderColor = System.Drawing.Color.Black;
+            this.myButton3.BorderRadius = 40;
+            this.myButton3.BorderSize = 1;
+            this.myButton3.FlatAppearance.BorderSize = 0;
+            this.myButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.myButton3.ForeColor = System.Drawing.Color.White;
+            this.myButton3.Location = new System.Drawing.Point(13, 167);
+            this.myButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.myButton3.Name = "myButton3";
+            this.myButton3.Size = new System.Drawing.Size(215, 69);
+            this.myButton3.TabIndex = 2;
+            this.myButton3.Text = "Divide";
+            this.myButton3.TextColor = System.Drawing.Color.White;
+            this.myButton3.UseVisualStyleBackColor = false;
+            // 
+            // myButton4
+            // 
+            this.myButton4.BackColor = System.Drawing.Color.MediumPurple;
+            this.myButton4.BackgroundColor = System.Drawing.Color.MediumPurple;
+            this.myButton4.BorderColor = System.Drawing.Color.Black;
+            this.myButton4.BorderRadius = 40;
+            this.myButton4.BorderSize = 1;
+            this.myButton4.FlatAppearance.BorderSize = 0;
+            this.myButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.myButton4.ForeColor = System.Drawing.Color.White;
+            this.myButton4.Location = new System.Drawing.Point(13, 244);
+            this.myButton4.Margin = new System.Windows.Forms.Padding(4);
+            this.myButton4.Name = "myButton4";
+            this.myButton4.Size = new System.Drawing.Size(215, 69);
+            this.myButton4.TabIndex = 3;
+            this.myButton4.Text = "Multiply";
+            this.myButton4.TextColor = System.Drawing.Color.White;
+            this.myButton4.UseVisualStyleBackColor = false;
+            // 
+            // round1
+            // 
+            this.round1.BackColor = System.Drawing.Color.MediumPurple;
+            this.round1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.round1.Location = new System.Drawing.Point(309, 20);
+            this.round1.Multiline = true;
+            this.round1.Name = "round1";
+            this.round1.Size = new System.Drawing.Size(472, 62);
+            this.round1.TabIndex = 4;
+            this.round1.TextChanged += new System.EventHandler(this.round1_TextChanged);
             // 
             // form1
             // 
@@ -56,7 +130,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.Indigo;
-            this.ClientSize = new System.Drawing.Size(1171, 850);
+            this.ClientSize = new System.Drawing.Size(868, 488);
+            this.Controls.Add(this.round1);
+            this.Controls.Add(this.myButton4);
+            this.Controls.Add(this.myButton3);
+            this.Controls.Add(this.myButton2);
             this.Controls.Add(this.myButton1);
             this.Font = new System.Drawing.Font("Segoe UI Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -65,11 +143,16 @@
             this.Text = "Calculator V2";
             this.Load += new System.EventHandler(this.form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private MyButton myButton1;
+        private MyButton myButton2;
+        private MyButton myButton3;
+        private MyButton myButton4;
+        private round round1;
     }
 }
--- Simple_CalculatorV2/Form1.cs
+++ Simple_CalculatorV2/Form1.cs
@@ -1,3 +1,7 @@
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
 namespace Simple_CalculatorV2
 {
     public partial class form1 : Form
@@ -9,15 +13,34 @@ namespace Simple_CalculatorV2
 
         private void form1_Load(object sender, EventArgs e)
         {
-            
+
 
         }
 
-        private void myButton1_Click(object sender, EventArgs e)
+        private void myButton1_Click_1(object sender, EventArgs e)
         {
 
         }
 
-        
+        private void myButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void round1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
+        
+
+
+
+
+
